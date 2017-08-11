8fac034ba5210e0040e2b371bff247c2f98e7475
tuxotron <rafael@troncoso.org>
2017-08-11 10:50:16 -0400

    set timezone

--- src/main/java/calories/tracker/app/dto/MealDTO.java
+++ src/main/java/calories/tracker/app/dto/MealDTO.java
@@ -22,7 +22,7 @@ public class MealDTO {
 
     private Long id;
 
-    @JsonFormat(pattern = "yyyy/MM/dd")
+    @JsonFormat(pattern = "yyyy/MM/dd", timezone = "EST")
     private Date date;
 
     @JsonSerialize(using = CustomTimeSerializer.class)
